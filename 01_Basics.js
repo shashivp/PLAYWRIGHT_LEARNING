6d52d2d8285fcdb3ec84cb41e59145b749294732
shashivp <vpshashi01@gmail.com>
2026-05-07 09:52:56 +0530

    first commit

---
+++ 01_Basics.js
@@ -0,0 +1,10 @@
+console.log("First code in js");
+
+//ctrl + l -> to clear terminal history.
+//ctrl + c -> to stop the execution of code in terminal.
+//ctrl + d -> to exit from terminal.
+//ctrl + z -> to stop the execution of code in terminal and move it to background. (to bring it back to foreground, use command 'fg' in terminal).
+//ctrl + a -> to move cursor to the beginning of the line in terminal.
+//ctrl + e -> to move cursor to the end of the line in terminal.
+
+//to run js file in terminal, use command 'node <filename>' in terminal. (e.g. 'node 01_Basics.js').
---
+++ Verify_setup.js
@@ -0,0 +1,9 @@
+//architecture
+console.log(process.arch);
+
+//operating system
+console.log(process.platform);
+
+//node version
+console.log(process.version);
+
