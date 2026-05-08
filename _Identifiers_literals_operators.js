a7fa62eba1f245ee15b61f15579ec2bde7bdba82
shashivp <vpshashi01@gmail.com>
2026-05-08 22:39:05 +0530

    Add Playwright learning code

---
+++ _Identifiers_literals_operators.js
@@ -0,0 +1,13 @@
+var a = 10;
+console.log(a);
+
+//a is an identifier 
+
+// 10 is a literal 
+
+// = is an operator
+
+//rule of identifier:
+//1. It can contain letters, digits, underscores and dollar signs
+//2. It must begin with a letter, underscore or dollar sign
+//3. It cannot be a reserved keyword
